f4861c8d83079dc90ad2fff44b5521d06801197c
Omri <irmoomri@gmail.com>
2024-07-18 15:40:53 +0300

    changed css, added artwork, changed the score input

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const releaseYearBox = document.getElementById("releaseYearBox");
 const genreBox = document.getElementById("genreBox");
 const scoreBox = document.getElementById("scoreBox");
 
+const apiKey = "1073c46f5c1453cc6c175d230afd704f"; //Last.fm Key for the auto album cover
+
 let allAlbums = [];
 
-function addNewAlbum() {
+async function addNewAlbum() {
     if (!validation()) return;
     const albumName = albumNameBox.value;
     const artistName = artistNameBox.value;
@@ -14,17 +16,21 @@ function addNewAlbum() {
     const releaseYear = +releaseYearBox.value;
     const score = +scoreBox.value;
 
-    const album = { artistName, albumName, releaseYear, genre, score };
+    const photoUrl = await fetchAlbumCover(artistName, albumName);
+
+    const album = { artistName, albumName, releaseYear, genre, score, photoUrl };
 
     allAlbums.push(album);
     dynamicTable(allAlbums);
     saveAlbums();
 }
 
+
 function dynamicTable(allAlbums) {
-    let html = `<table class="table table-bordered table-striped">
-    <thead class="thead-dark">
+    let html = `<table class="roboto-regular">
+    <thead>
     <tr>
+    <th>Album Cover</th>
     <th>Artist Name</th>
     <th>Album Name</th>
     <th>Year</th>
@@ -38,18 +44,20 @@ function dynamicTable(allAlbums) {
     for (let i = 0; i < allAlbums.length; i++) {
         html += `
             <tr>
+            <td><img class="album-cover" src="${allAlbums[i].photoUrl}" alt="${allAlbums[i].albumName}"></td>
         <td>${allAlbums[i].artistName}</td>
         <td>${allAlbums[i].albumName}</td>
         <td>${allAlbums[i].releaseYear}</td>
         <td>${allAlbums[i].genre}</td>
         <td>${allAlbums[i].score}</td>
-        <td><button class="btn btn-danger" onclick="deleteAlbum(${i})">Delete</button></td>
-        <td><button class="btn btn-primary btn-block" onclick="editAlbum(${i})">Edit</button></td>
+        <td><button class="" title="Delete Entry" onclick="deleteAlbum(${i})"><img src="assets/trash.svg"></button></td>
+        <td><button class="" title="Edit Entry" onclick="editAlbum(${i})"><img src="assets/pen.svg"></button></td>
         </tr>`;
     }
     html += `<tbody></table>`;
     containerDiv.innerHTML = html;
 }
+
 
 function deleteAlbum(index) {
     const sure = confirm(`Are you sure you want to delete the album?`);
@@ -73,15 +81,18 @@ function loadAlbums() {
 }
 
 function editAlbum(index) {
+    const sure = confirm("Are you sure you want to edit the album details?");
+    if(!sure) return;
     const album = allAlbums[index];
     albumNameBox.value = album.albumName;
     artistNameBox.value = album.artistName;
     releaseYearBox.value = album.releaseYear;
     genreBox.value = album.genre;
     scoreBox.value = album.score;
+    allAlbums.splice(index, 1);
+    dynamicTable(allAlbums);
 
-    document.getElementById("saveEditButton").setAttribute("onclick", `saveEdit(${index})`);
-    document.getElementById("editForm").style.display = "block";
+    saveAlbums();
 }
 
 function validation() {
@@ -111,4 +122,31 @@ function validation() {
         return false;
     }
     return true;
+}
+
+async function fetchAlbumCover(artist, album) {
+    try {
+        const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${apiKey}&artist=${artist}&album=${album}&format=json`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        if (data.album && data.album.image && data.album.image.length > 0) {
+            return data.album.image[data.album.image.length - 1]['#text'];
+        } else {
+            return 'default-image-url'; // A default image URL in case the API doesn't return an image
+        }
+    } catch (error) {
+        console.error('Failed to fetch album cover:', error);
+        return 'default-image-url'; // A default image URL in case of an error
+    }
+}
+
+
+function formReset(){
+    artistNameBox.value = ``;
+    albumNameBox.value = ``;
+    genreBox.value = ``;
+    scoreBox.value = 5;
+    releaseYearBox.value = ``;
 }
